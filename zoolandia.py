41f0b105a097655ce5de718c2ce552abb319fd2d
Hamilton Harris <hamilton.carter.harris@gmail.com>
2016-07-12 11:57:35 -0500

    first commit

---
+++ zoolandia.py
@@ -0,0 +1,22 @@
+class Animal:
+
+  def __init__(self):
+    self.species = None
+    self.name = ''
+
+
+
+class Species:
+
+  def __init__(self):
+    self.common_name = ''
+    self.geo_region = ''
+
+
+
+class Habitat:
+
+  def __init__(self):
+    self.name = ''
+    self.members = set()
+
